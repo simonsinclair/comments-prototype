0ae83448876ea3d1909586cf545ad72f72428537
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-05-22 17:04:23 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
-  <div id="app">
-    <!-- ... -->
+  <div id="app" class="app">
+    <bbc-comments :display-name="displayName"></bbc-comments>
   </div>
 </template>
 
 <script>
+import BbcComments from './BbcComments';
+
 export default {
   name: 'App',
-  components: {},
+  components: { BbcComments },
+  data() {
+    return {
+      displayName: 'Test User',
+    };
+  },
 };
 </script>
 
 <style lang="scss">
-
+  body {
+    background: #EEE;
+    padding-top: 72px;
+  }
 </style>
