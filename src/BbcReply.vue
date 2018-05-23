133a23731273ad6466e571dba731e400f46903d1
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-05-23 15:13:30 +0100

    Add BbcReply.vue

---
+++ src/BbcReply.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="reply gel-layout">
+    <div class="gel-layout__item">
+      <p>I'm a reply.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped="">
+  .reply {}
+</style>
