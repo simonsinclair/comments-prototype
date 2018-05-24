d482f728b9352a532c0851f15779ceeda86c74cb
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-05-24 14:51:55 +0100

    Update BbcReply.vue

--- src/BbcReply.vue
+++ src/BbcReply.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="reply gel-layout">
     <div class="gel-layout__item">
-      <p>I'm a reply.</p>
+      <p>{{ replyText }}</p>
     </div>
   </div>
 </template>
 
 <script>
-
+export default {
+  props: {
+    displayName: String,
+    replyText: String,
+    numUpVotes: Number,
+    numDownVotes: Number,
+  },
+};
 </script>
 
 <style lang="scss" scoped="">
