bcc2386af00512f4287a06aa9453c419d992a1d9
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-05-31 11:53:24 +0100

    Add BbcContributor.vue

---
+++ src/BbcContributor.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contributor">
+    <img class="contributor__avatar" :src="avatarSrc" alt="" width="32" height="32" />
+    <span class="contributor__name gel-pica-bold">{{ displayName }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    displayName: String,
+    avatarSrc: {
+      type: String,
+      default: 'https://placehold.it/64',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped="">
+  .contributor {
+    display: flex;
+    align-items: center;
+  }
+  .contributor__avatar {
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+  .contributor__name {}
+</style>
