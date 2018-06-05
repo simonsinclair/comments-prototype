2a0474acbd6444361b5f208d69999b54ce5780a7
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-06-05 14:30:01 +0100

    Tidy up

--- src/BbcComment.vue
+++ src/BbcComment.vue
@@ -78,28 +78,15 @@
 <script>
 import moment from 'moment';
 
-moment.updateLocale('en', {
-    relativeTime : {
-        // future: "in %s",
-        // past:   "%s ago",
-        s  : 'just now',
-        // ss : '%d seconds',
-        // m:  "a minute",
-        // mm: "%d minutes",
-        // h:  "an hour",
-        // hh: "%d hours",
-        // d:  "a day",
-        // dd: "%d days",
-        // M:  "a month",
-        // MM: "%d months",
-        // y:  "a year",
-        // yy: "%d years"
-    },
-});
-
 import BbcReply from './BbcReply';
 import BbcSubmitComment from './BbcSubmitComment';
 import BbcContributor from './BbcContributor';
+
+moment.updateLocale('en', {
+  relativeTime: {
+    s: 'just now',
+  },
+});
 
 export default {
   components: { BbcReply, BbcSubmitComment, BbcContributor },
--- src/BbcReply.vue
+++ src/BbcReply.vue
@@ -32,26 +32,13 @@
 <script>
 import moment from 'moment';
 
-moment.updateLocale('en', {
-    relativeTime : {
-        // future: "in %s",
-        // past:   "%s ago",
-        s  : 'just now',
-        // ss : '%d seconds',
-        // m:  "a minute",
-        // mm: "%d minutes",
-        // h:  "an hour",
-        // hh: "%d hours",
-        // d:  "a day",
-        // dd: "%d days",
-        // M:  "a month",
-        // MM: "%d months",
-        // y:  "a year",
-        // yy: "%d years"
-    },
-});
-
 import BbcContributor from './BbcContributor';
+
+moment.updateLocale('en', {
+  relativeTime: {
+    s: 'just now',
+  },
+});
 
 export default {
   components: { BbcContributor },
